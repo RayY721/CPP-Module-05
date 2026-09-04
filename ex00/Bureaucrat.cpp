41a8034c0ae0ecf74ac48911a10c9c6db2c092d9
RayY721 <74128049+RayY721@users.noreply.github.com>
2026-09-04 21:55:41 +0800

    simplified the usage of the exception.

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: kuyu <kuyu@student.codam.nl>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/31 09:29:47 by kuyu              #+#    #+#             */
-/*   Updated: 2026/09/01 17:35:12 by kuyu             ###   ########.fr       */
+/*   Updated: 2026/09/04 21:54:43 by kuyu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ Bureaucrat::Bureaucrat() : _name("Default"), _grade(150)
 Bureaucrat::Bureaucrat(const std::string& name, int grade) : _name(name), _grade(grade)
 {
 	if (grade < 1)
-		throw Bureaucrat::GradeTooHighException();
-	else if (grade > 150)
-		throw Bureaucrat::GradeTooLowException();
+		throw GradeTooHighException();
+	if (grade > 150)
+		throw GradeTooLowException();
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat& other) : _name(other._name), _grade(other._grade)
@@ -54,14 +54,14 @@ int	Bureaucrat::getGrade() const
 void	Bureaucrat::gradeIncrement()
 {
 	if (_grade <= 1)
-		throw Bureaucrat::GradeTooHighException();
+		throw GradeTooHighException();
 	_grade--;
 }
 
 void	Bureaucrat::gradeDecrement()
 {
 	if (_grade >= 150)
-		throw Bureaucrat::GradeTooLowException();
+		throw GradeTooLowException();
 	_grade++;
 }
 
